c3c8ade14f4c15fe73e76745ed0c91ac3006f2bc
Togotogo98 <rimpaipb2014@gmail.com>
2026-07-14 22:33:41 +0100

    Phase 3: Added future-based results to SubmitTask using packaged_task

--- Include/ThreadPool.h
+++ Include/ThreadPool.h
@@ -17,6 +17,9 @@
 #include <queue>
 #include <condition_variable>
 #include <functional>
+#include <future>
+#include <stdexcept>
+#include <memory>
 
 
 class ThreadPool
@@ -44,25 +47,59 @@ public:
      *           (lambda, function pointer, functor, etc.)
      * Arguments : the types of whatever extra arguments that
      *             callable needs when it is eventually invoked
+     * ReturnType : Whatever Callable(Arguments...) would return when 
+     *              invoked
+     * 
      * */
     template<typename Callable, typename... Arguments>
-    void SubmitTask(Callable&& callable, Arguments&&... arguments)
+    auto SubmitTask(Callable&& callable, Arguments&&... arguments)
+        -> std::future<std::invoke_result_t<Callable, Arguments...>>
     {
+        /* std::result_of<Callable(Arguments...)>::type - not using the C++11 version.
+         * C++17 version is std::invoke_result_t */
+        using ReturnType = std::invoke_result_t<Callable, Arguments...>;
         /* 
          * std::bind packages the callable together with its arguments
-         * into a single object, "boundTask", that takes ZERO arguments
-         * when invoked later. This is required because the task queue
+         * into a single object "task", that takes ZERO arguments
+         * when invoked later. This is required because the tasks queue
          * only knows how to store std::function<void()> objects. 
-         * (note: function<void()> is a callable that takes no arguments and returns nothing)
+         * (note: function<void()> is a callable that takes no arguments and returns nothing
+         * hence the need to bind the callable with its arguments into a single object that 
+         * takes no arguments)
          *
          * std::forward preserves whether the original callable/arguments
          * were temporaries, avoiding unnecessary copies. 
          */
-        auto task = std::bind(std::forward<Callable>(callable), std::forward<Arguments>(arguments)...);
+        auto task = std::bind(std::forward<Callable>(callable), 
+                            std::forward<Arguments>(arguments)...);
+
+        /* Create a packaged task and store it in a shared pointer.
+         * The shared pointer is used to allow the task to be copied into the lambda
+         * without affecting the original packaged task. Lambda is a copy of the packaged task, 
+         * and when the lambda is executed, it will invoke the packaged task.
+         * 
+         * A packaged task is a wrapper for a callable (like a function or lambda) that
+         * retrieves the result of the callable asynchronously using a future.
+         * When packaged task is created, a future gets associated with it.
+         * When the packaged task is executed, it runs the callable and sets the result
+         * in the future, which can be retrieved later.
+         */
+        auto packagedTaskPtr = std::make_shared<std::packaged_task<ReturnType()>>(std::move(task));
+
+        std::future<ReturnType> resultFuture = packagedTaskPtr->get_future();
+
         {
             std::unique_lock<std::mutex> lock(queue_mutex);
-            tasks.emplace(task);
+
+            if (stop)
+                throw std::runtime_error("SubmitTask called on a stopped ThreadPool");
+
+            tasks.emplace([packagedTaskPtr]()
+            {
+                (*packagedTaskPtr)();
+            });
         }
         condition.notify_one();
+        return resultFuture;
     }
 };
--- Src/main.cpp
+++ Src/main.cpp
@@ -6,24 +6,18 @@
 std::mutex print_mutex;
 
 /* Add task - function */
-void Sum(int firstNumber, int secondNumber)
+int Sum(int firstNumber, int secondNumber)
 {
-    std::lock_guard<std::mutex> lock(print_mutex);
-    std::cout << firstNumber << " + " << secondNumber << " = "
-            << (firstNumber + secondNumber)
-            << " (thread " << std::this_thread::get_id() << ")" << std::endl;
+    return firstNumber + secondNumber;
 }
 
 /* Multiply task - class */
 class Multiplier
 {
 public:
-    void operator()(int firstNumber, int secondNumber) const
+    int operator()(int firstNumber, int secondNumber) const
     {
-        std::lock_guard<std::mutex> lock(print_mutex);
-        std::cout << firstNumber << " * " << secondNumber << " = "
-                << (firstNumber * secondNumber)
-                << " (thread " << std::this_thread::get_id() << ")" << std::endl;
+        return firstNumber * secondNumber;
     }
 };
 
@@ -34,26 +28,54 @@ int main()
 
     ThreadPool pool(4);
 
-    /* Dummy tasks for testing */
+    /* Vector to hold futures for the sum tasks */
+    std::vector<std::future<int>> sumFutures;
+
+    /* Submit tasks to the thread pool */
     for (int i = 0; i < 8; i++)
     {
-        pool.SubmitTask([i]
-                        {
-                            std::lock_guard<std::mutex> lock(print_mutex);
-                            std::cout << "Task " << i << " running on thread "
-                                    << std::this_thread::get_id() << std::endl;
-                        });
+        std::future<int> sumResultFuture = pool.SubmitTask(Sum, i, i * 10);
+        sumFutures.push_back(std::move(sumResultFuture));
+
+        std::lock_guard<std::mutex> lock(print_mutex);
+        std::cout << "Submitted task " << i 
+                << " on thread " 
+                << std::this_thread::get_id() 
+                << std::endl;
     }
 
-    for (int i = 0; i < 4; i++)
+    for (size_t i = 0; i < sumFutures.size(); i++)
     {
-        pool.SubmitTask(Sum, i, i + 1);
+        int result = sumFutures[i].get();
+        std::lock_guard<std::mutex> lock(print_mutex);
+        std::cout << "Result " << i << ": " << result << std::endl;
     }
 
     Multiplier multiplier;
     for (int i = 0; i < 4; i++)
     {
-        pool.SubmitTask(multiplier, i, i + 2);
+        std::future<int> multiplyResultFuture = pool.SubmitTask(multiplier, i, i + 1);
+        int result = multiplyResultFuture.get();
+        std::lock_guard<std::mutex> lock(print_mutex);
+        std::cout << "Multiplication Result " << i << ": " << result << std::endl;
+    }
+
+    //Making this program's life a little more complicated because I'm evil.
+    /* Combination of tasks Sum and Multiplier -  Square of sums */
+    std::vector<std::future<int>> combinationFutures;
+    for (int i = 0; i < 10; i++)
+    {
+        auto sumFuture = pool.SubmitTask(Sum, i, i + 1);
+        int sumResult = sumFuture.get();
+        auto SquareFuture = pool.SubmitTask(multiplier, sumResult, sumResult);
+        combinationFutures.push_back(std::move(SquareFuture));
+    }
+
+    for (size_t i = 0; i < combinationFutures.size(); i++)
+    {
+        int result = combinationFutures[i].get();
+        std::lock_guard<std::mutex> lock(print_mutex);
+        std::cout << "Combination Result " << i << ": " << result << std::endl;
     }
 
     return 0;
